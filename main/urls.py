85d1fd5525637d3b8d56c5e6de8f5127ae8c8a15
koipish <koipish@gmail.com>
2024-04-05 20:46:05 +0300

    modified

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from main.views import index, contact
+from main.apps import MainConfig
 
+app_name = MainConfig.name
 urlpatterns = [
-    path('', index),
-    path('contact/', contact)
+    path('', index, name='index'),
+    path('contact/', contact, name='contact')
 ]
